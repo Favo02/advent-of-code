67fc91a087731f7b8d9b9d17ded986bcce37e58e
Luca Favini <luca.favini.02@gmail.com>
2022-12-09 16:43:24 +0100

    2022-day9: refactor

--- 2022/day9/ropeBridges.go
+++ 2022/day9/ropeBridges.go
@@ -21,8 +21,8 @@ func main() {
 
 	instructions := parseInput()
 
-	var rope []Point
-	var crossed map[Point]bool
+	var rope []Point           // current points of the rope
+	var crossed map[Point]bool // points crossed by the tail (last point) of the rope
 
 	// execute 2 times, one for part1, one for part2
 	for i := 0; i < 2; i++ {
@@ -37,10 +37,10 @@ func main() {
 		rope = make([]Point, tail)     // current rope points positions
 		crossed = make(map[Point]bool) // points crossed by the tail of the rope
 
-		crossed[Point{0, 0}] = true
+		crossed[Point{0, 0}] = true // starting point is crossed by every point of the rope
 
 		for _, instruction := range instructions {
-			moveHead(instruction, rope, crossed, tail)
+			moveHead(instruction, rope, crossed, tail) // move head in the direction of instruction (the tail of the rope will follow the head)
 		}
 		fmt.Print("Points crossed by the tail (of length ", tail, "):\n\t", len(crossed), "\n")
 	}
@@ -65,9 +65,9 @@ func moveHead(instruction string, rope []Point, crossed map[Point]bool, tailLeng
 	tokens := strings.Split(instruction, " ")
 	direction := tokens[0]
 	amount, _ := strconv.Atoi(tokens[1])
-	for i := 0; i < amount; i++ {
-		moveStep(direction, rope)
-		crossed[rope[tailLength-1]] = true
+	for i := 0; i < amount; i++ { // move the head the amount of times the instruction requires
+		moveStep(direction, rope)          // moves the head in the direction 1 time
+		crossed[rope[tailLength-1]] = true // mark as crossed the point the rope tail is currently
 	}
 
 	return rope, crossed
@@ -78,18 +78,19 @@ func moveHead(instruction string, rope []Point, crossed map[Point]bool, tailLeng
 func moveStep(dir string, rope []Point) []Point {
 	switch dir {
 	case "U":
-		rope[0].y--
+		rope[0].y-- // move head up
 	case "D":
-		rope[0].y++
+		rope[0].y++ // move head down
 	case "L":
-		rope[0].x--
+		rope[0].x-- // move head left
 	case "R":
-		rope[0].x++
+		rope[0].x++ // move head right
 	}
 
-	for i := 1; i < len(rope); i++ {
-		if !isTailClose(rope[i], rope[i-1]) {
-			rope = moveTailPoint(rope, i, i-1)
+	// make tail points follow the head
+	for i := 1; i < len(rope); i++ { // scan every point of the rope (except the head)
+		if !isTailClose(rope[i], rope[i-1]) { // if the point is not close to his predecessor
+			rope = moveTailPoint(rope, i, i-1) // move the point close to his predecessor
 		}
 	}
 
@@ -99,8 +100,10 @@ func moveStep(dir string, rope []Point) []Point {
 // REQUIRES: currentPoint, targetPoint not nil
 // EFFECTS: returns true if target point is close (if it is tha same point or if it is in one of the 8 cells surrounding the point), false otherwise
 func isTailClose(currentPoint, targetPoint Point) bool {
+	// scan each of the 8 (+ the same point) surrounding the targetPoint
 	for i := targetPoint.y - 1; i < targetPoint.y+2; i++ {
 		for j := targetPoint.x - 1; j < targetPoint.x+2; j++ {
+			// if the current point is in this 9 points then it is close to targetPoint
 			if i == currentPoint.y && j == currentPoint.x {
 				return true
 			}
@@ -112,7 +115,7 @@ func isTailClose(currentPoint, targetPoint Point) bool {
 // REQUIRES: rope is not nil, currentIndex and targetIndex are valid indexes of rope
 // EFFECTS: returns the rope updated with the point at "currentIndex" moved if it is not close to "targetIndex" point
 func moveTailPoint(rope []Point, currentIndex, targetIndex int) []Point {
-	// same column
+	// same column, so move point only horizontally
 	if rope[targetIndex].x == rope[currentIndex].x {
 		if rope[targetIndex].y > rope[currentIndex].y {
 			rope[currentIndex].y++
@@ -122,7 +125,7 @@ func moveTailPoint(rope []Point, currentIndex, targetIndex int) []Point {
 		return rope
 	}
 
-	// same row
+	// same row, so move point only vertically
 	if rope[targetIndex].y == rope[currentIndex].y {
 		if rope[targetIndex].x > rope[currentIndex].x {
 			rope[currentIndex].x++
@@ -132,7 +135,7 @@ func moveTailPoint(rope []Point, currentIndex, targetIndex int) []Point {
 		return rope
 	}
 
-	// diagonal
+	// not same row or same column, so move point diagonally
 	if rope[currentIndex].x < rope[targetIndex].x && rope[currentIndex].y < rope[targetIndex].y {
 		rope[currentIndex].x++
 		rope[currentIndex].y++
