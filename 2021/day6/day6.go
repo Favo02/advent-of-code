875b990b64d9c30580a6cfdc0d4d6ad37a513dcb
Luca Favini <luca.favini.02@gmail.com>
2022-12-10 00:12:13 +0100

    2021-day: refactor

--- 2021/day6/day6.go
+++ 2021/day6/day6.go
@@ -11,64 +11,61 @@ import (
 func main() {
 
 	// read starting fishes
-	pesciStr := ""
-	Scan(&pesciStr)
-	pesci := strings.Split(pesciStr, ",")
+	fishesStr := ""
+	Scan(&fishesStr)
+	fishes := strings.Split(fishesStr, ",")
 
 	// parse strings slice to ints slice
-	var pesciInt = []int{}
-	for _, i := range pesci {
-		j, err := strconv.Atoi(i)
-		if err != nil {
-			panic(err)
-		}
-		pesciInt = append(pesciInt, j)
+	var fishesInt = []int{}
+	for _, i := range fishes {
+		j, _ := strconv.Atoi(i)
+		fishesInt = append(fishesInt, j)
 	}
 
 	// current day
-	var giorno int = 1
+	var curDay int = 1
 
 	// number of days
-	var giorni int
+	var days int
 	if len(os.Args) < 2 {
 		fmt.Println("insert number of days to calculate as command line argument")
 		return
 	}
-	giorniStr := os.Args[1]
-	giorni, _ = strconv.Atoi(giorniStr)
+	daysStr := os.Args[1]
+	days, _ = strconv.Atoi(daysStr)
 
 	// calculate starting statuses of starting fishes
-	num_pesci_array := printUniqueValue(pesciInt)
-	num_pesci := num_pesci_array[:] //array to slice
+	fishesNumArray := printUniqueValue(fishesInt)
+	fishesNum := fishesNumArray[:] //array to slice
 
 	// for each day
-	for giorno <= giorni {
+	for curDay <= days {
 		// fishes at status 0 are the only one that do something
-		num_pesci_a_zero := num_pesci[0]
+		zeroFishes := fishesNum[0]
 
 		// shift left fish slice (fish in status 8 --> status 7, status 7 --> 6, ...)
 		// move fishes in status 0 to status 8 (new fishes generated)
-		num_pesci = append(num_pesci[1:], num_pesci_a_zero)
+		fishesNum = append(fishesNum[1:], zeroFishes)
 		// move parent fishes (that just generated a fish) to status 6
-		num_pesci[6] += num_pesci_a_zero
+		fishesNum[6] += zeroFishes
 
-		giorno++
+		curDay++
 	}
-	Println(sommaSlice(num_pesci))
+	Println(sumSlice(fishesNum))
 
 }
 
 // returns number of fishes in each status
 func printUniqueValue(arr []int) [9]int {
-	var num_pesci [9]int
+	var fishesNum [9]int
 	for _, v := range arr {
-		num_pesci[v]++
+		fishesNum[v]++
 	}
-	return num_pesci
+	return fishesNum
 }
 
 // returns the sum of slice
-func sommaSlice(sl []int) int {
+func sumSlice(sl []int) int {
 	res := 0
 	for _, v := range sl {
 		res += v
