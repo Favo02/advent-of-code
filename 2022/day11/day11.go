e6a499fa70d8eb5f8a43e56f41ef25891c44927b
Luca Favini <luca.favini.02@gmail.com>
2022-12-10 23:42:17 +0100

    2022-day11: template

---
+++ 2022/day11/day11.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	lines := parseInput()
+	fmt.Println(lines)
+}
+
+func parseInput() (lines []string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
