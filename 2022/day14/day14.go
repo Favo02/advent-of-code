ad5bbbe2b1ae363540b68f44b5bbc92a20edfdc3
Luca Favini <luca.favini.02@gmail.com>
2022-12-14 11:27:21 +0100

    2022-day14: refactor

--- 2022/day14/day14.go
+++ 2022/day14/day14.go
@@ -15,15 +15,15 @@ type Point struct {
 	x, y int
 }
 
-var sandStart Point = Point{500, 0}
+var sandSpawn Point = Point{500, 0}
 
 func main() {
 	rocks := parseInput()
 	cave1 := fillRocks(rocks) // fill lines between rocks
 	cave2 := copyMap(cave1)   // duplicate cave for part2
 
-	cave1 = simulateSand(cave1, false) // simulate falling sand (without floor)
-	cave2 = simulateSand(cave2, true)  // simulate falling sand (with floor)
+	cave1 = fillSand(cave1, false) // simulate falling sand (without floor)
+	cave2 = fillSand(cave2, true)  // simulate falling sand (with floor)
 
 	sand1 := countSand(cave1) // count sand stuck in cave
 	sand2 := countSand(cave2) // count sand stuck in cave
@@ -32,6 +32,9 @@ func main() {
 	fmt.Println("sand standing on floor (part2):\n\t", sand2)
 }
 
+// REQUIRES: stdin is a valid challenge input
+// MODIFIES: stdin
+// EFFECTS: returns each line of rocks
 func parseInput() (rocks [][]Point) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -51,16 +54,20 @@ func parseInput() (rocks [][]Point) {
 	return rocks
 }
 
+// REQUIRES: distance between two rocks on the same line must be one dimensional (only x or only y)
+// EFFECTS: returns a map (cave) on which is represented each point that is a rock
 func fillRocks(rocks [][]Point) map[Point]int {
 	cave := make(map[Point]int)
 	for _, rock := range rocks { // scan each rocks line
 		for i := 0; i < len(rock)-1; i++ { // scan each rock
-			drawLine(cave, rock[i], rock[i+1]) // draw rock line in map between two rocks
+			drawLine(cave, rock[i], rock[i+1]) // set as rock on cave each point between two consecutive rocks (on the same line)
 		}
 	}
 	return cave
 }
 
+// REQUIRES: distance between "source" and "destination" must be one dimensional (only x or only y)
+// EFFECTS: returns cave on which has been set as rocks each point between source and destination
 func drawLine(cave map[Point]int, source, destination Point) map[Point]int {
 	diffX := source.x - destination.x
 	diffY := source.y - destination.y
@@ -90,21 +97,22 @@ func drawLine(cave map[Point]int, source, destination Point) map[Point]int {
 	return cave
 }
 
-func simulateSand(cave map[Point]int, floor bool) map[Point]int {
+// EFFECTS: returns the cave on which have been set as sand the points on which sand is stuck after falling from spawn
+func fillSand(cave map[Point]int, floor bool) map[Point]int {
 	var oldSand int
 	for true {
 		// sand spawn blocked
-		if cave[sandStart] == -1 {
+		if cave[sandSpawn] == -1 {
 			break
 		}
 
 		// spawn new sand
-		cave[sandStart] = -1
+		cave[sandSpawn] = -1
 
 		// let sand spawned fall
-		cave = sandFall(cave, sandStart, floor)
+		cave = sandFall(cave, sandSpawn, floor)
 
-		// if sand spawned falled out of map
+		// if sand spawned falled out of cave
 		if countSand(cave) == oldSand {
 			break
 		}
@@ -113,6 +121,7 @@ func simulateSand(cave map[Point]int, floor bool) map[Point]int {
 	return cave
 }
 
+// EFFECTS: return the cave on which a single point of sand ("curSand") have been moved to his final position
 func sandFall(cave map[Point]int, curSand Point, floor bool) map[Point]int {
 	lowestRock := lowestRock(cave) // lowest rock
 	var rest bool                  // is current sand piece at rest
@@ -126,7 +135,7 @@ func sandFall(cave map[Point]int, curSand Point, floor bool) map[Point]int {
 				break
 			}
 		} else {
-			// below lowest rock: falling out of the map
+			// below lowest rock: falling out of the cave
 			if curSand.y == lowestRock {
 				cave[curSand] = 0
 				break
@@ -163,6 +172,7 @@ func sandFall(cave map[Point]int, curSand Point, floor bool) map[Point]int {
 	return cave
 }
 
+// EFFECTS: returns the number of sand points in the cave
 func countSand(cave map[Point]int) int {
 	var sand int
 	for _, p := range cave {
@@ -173,6 +183,7 @@ func countSand(cave map[Point]int) int {
 	return sand
 }
 
+// EFFECTS: return the y position of the lowest rock in the cave
 func lowestRock(cave map[Point]int) int {
 	low := math.MinInt
 	for k, v := range cave {
@@ -183,9 +194,10 @@ func lowestRock(cave map[Point]int) int {
 	return low
 }
 
-func copyMap(cave map[Point]int) map[Point]int {
+// EFFECTS: returns a new map copy of "oldMap"
+func copyMap(oldMap map[Point]int) map[Point]int {
 	newMap := make(map[Point]int)
-	for k, v := range cave {
+	for k, v := range oldMap {
 		newMap[k] = v
 	}
 	return newMap
