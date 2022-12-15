3e7420d28752670236909f10a0a9d46916055b01
Luca Favini <luca.favini.02@gmail.com>
2022-12-15 13:48:00 +0100

    2022-day15: part2 refactor

--- 2022/day15/day15_part2.go
+++ 2022/day15/day15_part2.go
@@ -17,150 +17,68 @@ const FORCEDVOID int = -1
 
 const MAX int = 4000000
 
-func main() {
-	sensors := parseInput()
-	cave := placeSensorsBeacon(sensors)
-
-	borders := scanSensorBorder(sensors)
-
-	for borderPoint, v := range borders {
-
-		if !v {
-			continue
-		}
-
-		if cave[borderPoint] == SENSOR {
-			continue
-		}
-		if cave[borderPoint] == BEACON {
-			continue
-		}
-
-		close := false
-		// scan each sensor
-		for sen, bea := range sensors {
-
-			// distance from line point to sensor
-			thisdist := manhattanDistance(sen, borderPoint)
-
-			// distance from sensor to his beacon
-			sendist := manhattanDistance(sen, bea)
-
-			if thisdist <= sendist {
-				close = true
-				break
-			}
-		}
-
-		if !close {
-			if borderPoint.x > MAX || borderPoint.x < 0 {
-				continue
-			}
-			if borderPoint.y > MAX || borderPoint.y < 0 {
-				continue
-			}
-			fmt.Println(borderPoint)
-		}
-
-	}
-}
-
 type Point struct {
 	x, y int
 }
 
+func main() {
+	sensors := parseInput()
+	borderPoints := scanSensorBorder(sensors)
+	point := checkBorderPoints(sensors, borderPoints)
+
+	fmt.Println("score of the point not in reach of any sensor (part2):\n\t", point.x*MAX+point.y)
+}
+
+// returns each sensor associated to his beacon
 func parseInput() map[Point]Point {
-	cave := make(map[Point]Point)
+	sensors := make(map[Point]Point)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
 		tokens := strings.Split(line, " ")
 
-		sx, sy := tokens[2], tokens[3]
-		bx, by := tokens[8], tokens[9]
+		// get x,y as strings
+		senXstr, senYstr := tokens[2], tokens[3]
+		beaXstr, beaYstr := tokens[8], tokens[9]
 
-		sxx, _ := strconv.Atoi(sx[2 : len(sx)-1])
-		syy, _ := strconv.Atoi(sy[2 : len(sy)-1])
+		// parse sensor x,y to int
+		senX, _ := strconv.Atoi(senXstr[2 : len(senXstr)-1])
+		senY, _ := strconv.Atoi(senYstr[2 : len(senYstr)-1])
 
-		bxx, _ := strconv.Atoi(bx[2 : len(bx)-1])
-		byy, _ := strconv.Atoi(by[2:])
+		// parse beacon x,y to int
+		beaX, _ := strconv.Atoi(beaXstr[2 : len(beaXstr)-1])
+		beaY, _ := strconv.Atoi(beaYstr[2:])
 
-		cave[Point{sxx, syy}] = Point{bxx, byy}
+		// add sensor
+		sensors[Point{senX, senY}] = Point{beaX, beaY}
 	}
-	return cave
+	return sensors
 }
 
+// returns the points just outside of each sensor reach
+func scanSensorBorder(sensors map[Point]Point) []Point {
+	var borderPoints []Point
+	for s, v := range sensors {
+		dist := manhattanDistance(s, v)
+
+		// add points just out of each direction of the sensor reach ("draw" a diamond)
+		for i := dist; i >= 0; i-- {
+			borderPoints = append(borderPoints, Point{s.x + (dist - i), s.y + (dist - (dist - i)) + 1})
+			borderPoints = append(borderPoints, Point{s.x - (dist - i), s.y - (dist - (dist - i)) - 1})
+			borderPoints = append(borderPoints, Point{s.x + (dist - (dist - i)) + 1, s.y + (dist - i)})
+			borderPoints = append(borderPoints, Point{s.x - (dist - (dist - i)) - 1, s.y - (dist - i)})
+		}
+	}
+	return borderPoints
+}
+
+// returns the manhattand distance between "a" and "b"
 func manhattanDistance(a, b Point) int {
 	return abs(a.x-b.x) + abs(a.y-b.y)
 }
 
-func placeSensorsBeacon(sensors map[Point]Point) map[Point]int {
-	cave := make(map[Point]int)
-	for s, b := range sensors {
-		cave[s] = SENSOR
-		cave[b] = BEACON
-	}
-	return cave
-}
-
-func forcedVoidOnLine(sensors map[Point]Point, cave map[Point]int) (int, int) {
-	min := 0
-	max := MAX
-
-	// scan each point of line
-	for y := min; y < max; y++ {
-		for x := 0; x < max; x++ {
-
-			thispoint := Point{x, y}
-
-			if cave[thispoint] == SENSOR {
-				continue
-			}
-			if cave[thispoint] == BEACON {
-				continue
-			}
-
-			// scan each sensor
-			for sen, bea := range sensors {
-
-				// distance from line point to sensor
-				thisdist := manhattanDistance(sen, thispoint)
-
-				// distance from sensor to his beacon
-				sendist := manhattanDistance(sen, bea)
-
-				if thisdist <= sendist {
-					cave[thispoint] = FORCEDVOID
-					break
-				}
-			}
-		}
-
-		// check if line has void
-		index := lineContaints(cave, y)
-		if index >= 0 {
-			return index, y
-		}
-	}
-	return -1, -1
-}
-
-func scanSensorBorder(sensors map[Point]Point) map[Point]bool {
-	border := make(map[Point]bool)
-	for s, v := range sensors {
-		dist := manhattanDistance(s, v)
-
-		for i := dist; i >= 0; i-- {
-			border[Point{s.x + (dist - i), s.y + (dist - (dist - i)) + 1}] = true
-			border[Point{s.x - (dist - i), s.y - (dist - (dist - i)) - 1}] = true
-
-			border[Point{s.x + (dist - (dist - i)) + 1, s.y + (dist - i)}] = true
-			border[Point{s.x - (dist - (dist - i)) - 1, s.y - (dist - i)}] = true
-		}
-	}
-	return border
-}
-
+// returns the absolute value of "a"
 func abs(a int) int {
 	if a > 0 {
 		return a
@@ -168,11 +86,43 @@ func abs(a int) int {
 	return -(a)
 }
 
-func lineContaints(cave map[Point]int, line int) int {
-	for i := 0; i < MAX; i++ {
-		if cave[Point{i, line}] == VOID {
-			return i
+// returns the (first) point outside of each sensor reach in border points
+func checkBorderPoints(sensors map[Point]Point, borderPoints []Point) Point {
+	// scan each border point
+	for _, borderPoint := range borderPoints {
+
+		// if current border point outside of valid range skip
+		if borderPoint.x > MAX || borderPoint.x < 0 {
+			continue
+		}
+		if borderPoint.y > MAX || borderPoint.y < 0 {
+			continue
+		}
+
+		inReach := false // in reach of a sensor
+
+		// scan each sensor
+		for sen, bea := range sensors {
+
+			// distance from border point to sensor
+			thisdist := manhattanDistance(sen, borderPoint)
+
+			// distance from sensor to his beacon
+			sendist := manhattanDistance(sen, bea)
+
+			// if sensor to beacon distance is greather than border point to beacon then the current border point is in sensor range
+			if thisdist <= sendist {
+				inReach = true
+				break
+			}
+		}
+
+		// point not in reach of any sensor
+		if !inReach {
+			return borderPoint
 		}
 	}
-	return -1
+
+	// no points outside of every sensor range
+	return Point{}
 }
