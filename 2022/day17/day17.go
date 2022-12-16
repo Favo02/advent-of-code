48dcb60b4bdc34dd22b1c392a36a9de1bcd655bb
Luca Favini <luca.favini.02@gmail.com>
2022-12-16 22:32:33 +0100

    2022-day17: template

---
+++ 2022/day17/day17.go
@@ -0,0 +1,23 @@
+// https://adventofcode.com/2022/day/17
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	lines := parseInput()
+	fmt.Println(lines)
+}
+
+func parseInput() (lines []string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
