0fd13dd4e3ee1e2882809a645d8eac626646349b
Luca Favini <luca.favini.02@gmail.com>
2022-12-17 23:39:00 +0100

    2022-day17: refactor

--- 2022/day17/day17.go
+++ 2022/day17/day17.go
@@ -31,24 +31,20 @@ type StateValue struct {
 // rocks shapes
 var ROCKS [5]string = [5]string{"####", ".#.\n###\n.#.", "..#\n..#\n###", "#\n#\n#\n#", "##\n##"}
 
-var cave map[Point]bool = make(map[Point]bool)
-var fixed map[Point]bool = make(map[Point]bool)
+// walls of cave
+const WALLEFT int = 0
+const WALLRIGHT int = 8
 
-const WALLEFT, WALLRIGHT int = 0, 8
-
-var turn, gasIndex int
-
-var gas string
-
+// part1 and part2 rocks number
 const PART1 int = 2022
 const PART2 int = 1_000_000_000_000
 
-var statuses map[State]StateValue = make(map[State]StateValue)
+// index of gas affecting the rocks
 
 func main() {
-	gas = parseInput()
+	gas := parseInput()
 
-	p1, p2 := spawnRocks(1_000_000_000_000)
+	p1, p2 := spawnRocks(gas, 1_000_000_000_000)
 	fmt.Println(p1, p2)
 }
 
@@ -63,45 +59,51 @@ func parseInput() (gas string) {
 
 // modifies cave placing nRocks rocks,
 // returns the cave height at PART1 time and at PART2 time
-func spawnRocks(nRocks int) (int, int) {
+func spawnRocks(gas string, nRocks int) (int, int) {
+
+	cave := make(map[Point]bool)
+	fixed := make(map[Point]bool)
+
 	var part1res int
 	var foundCycle bool
 	var cycleHeight, steps int
+	var gasIndex int
+
+	statuses := make(map[State]StateValue)
 
 	for rocksSpawned := 0; rocksSpawned <= nRocks; rocksSpawned++ {
 		if rocksSpawned == PART1 {
-			part1res = highestRock()
+			part1res = highestRock(cave)
 		}
 
-		spawnRock(rocksSpawned % 5)
-		turn = 0
-		fallRock()
+		spawnRock(cave, rocksSpawned%5)
+		gasIndex = fallRock(cave, fixed, gas, gasIndex)
 
-		curState := getCurrentState(rocksSpawned%5 + 1)
+		curState := getCurrentState(cave, gas, gasIndex, rocksSpawned%5+1)
 
 		oldStatus, found := statuses[curState]
 		if rocksSpawned >= PART1 && found && !foundCycle {
 			foundCycle = true
 
 			cycle := rocksSpawned - oldStatus.rocks // number of rocks in the cycle
-			cycleHeight = highestRock() - oldStatus.height
+			cycleHeight = highestRock(cave) - oldStatus.height
 
 			steps = (PART2 - oldStatus.rocks) / cycle // steps needed to reach tot rocks
 
 			rocksSpawned = oldStatus.rocks + (cycle * steps)
 
 		} else {
-			statuses[curState] = StateValue{highestRock(), rocksSpawned}
+			statuses[curState] = StateValue{highestRock(cave), rocksSpawned}
 		}
 	}
-	return part1res, (highestRock() + ((steps - 1) * cycleHeight)) - 1
+	return part1res, (highestRock(cave) + ((steps - 1) * cycleHeight)) - 1
 }
 
 // modifies the cave, placing a rock (of shape of index t) on grid (at top of cave)
-func spawnRock(t int) {
+func spawnRock(cave map[Point]bool, t int) {
 	rock := ROCKS[t]
 
-	max := -highestRock()
+	max := -highestRock(cave)
 
 	spawn := Point{WALLEFT + 3, max - 4}
 
@@ -121,7 +123,7 @@ func spawnRock(t int) {
 }
 
 // returns the highest point of a rock
-func highestRock() int {
+func highestRock(cave map[Point]bool) int {
 	min := 0
 	for rock := range cave {
 		if rock.y < min {
@@ -132,7 +134,8 @@ func highestRock() int {
 }
 
 // modifies cave, letting the rocks fall to definite position
-func fallRock() {
+func fallRock(cave, fixed map[Point]bool, gas string, gasIndex int) int {
+	var turn int = 0
 	var overlap bool
 	for !overlap {
 
@@ -205,11 +208,12 @@ func fallRock() {
 		turn++
 		// visualizeMap()
 	}
+	return gasIndex
 }
 
 // modifies stdout printing a visualization of the cave
-func visualizeMap() {
-	for i := -highestRock(); i < 0; i++ {
+func visualizeMap(cave map[Point]bool) {
+	for i := -highestRock(cave); i < 0; i++ {
 		for j := 1; j < 8; j++ {
 			if cave[Point{j, i}] {
 				fmt.Print("█")
@@ -223,14 +227,14 @@ func visualizeMap() {
 }
 
 // returns the current state (generated on 40 top rocks levels)
-func getCurrentState(rock int) State {
-	curState := State{rock, gasIndex % len(gas), getTopRocks()}
+func getCurrentState(cave map[Point]bool, gas string, gasIndex, rock int) State {
+	curState := State{rock, gasIndex % len(gas), getTopRocks(cave)}
 	return curState
 }
 
 // returns the top 40 rocks
-func getTopRocks() (res string) {
-	topRockIndex := -highestRock()
+func getTopRocks(cave map[Point]bool) (res string) {
+	topRockIndex := -highestRock(cave)
 	for i := topRockIndex; i < topRockIndex+40; i++ {
 		for j := 1; j <= 7; j++ {
 			if cave[Point{j, i}] {
