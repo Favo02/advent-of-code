1aaabf9b6f030a99ec73a7f720c225aa78d82ee5
Luca Favini <luca.favini.02@gmail.com>
2022-12-20 21:40:49 +0100

    2022-day20: part2 working

---
+++ 2022/day20/day20_part2_original.go
@@ -0,0 +1,104 @@
+// https://adventofcode.com/2022/day/20
+// https://github.com/Favo02/advent-of-code
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+type Number struct {
+	value int
+	index int
+}
+
+func main() {
+	numbers := parseInput()
+	nums := orderNumbers(numbers, 10)
+	n1000, n2000, n3000 := get10002000300(nums)
+	fmt.Println(nums[n1000].value + nums[n2000].value + nums[n3000].value)
+}
+
+const DECRYPTION_KEY int = 811589153
+
+func parseInput() (nums []Number) {
+	scanner := bufio.NewScanner(os.Stdin)
+	var i int = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		n, _ := strconv.Atoi(line)
+		nums = append(nums, Number{n * DECRYPTION_KEY, i})
+		i++
+	}
+	return nums
+}
+
+func orderNumbers(nums []Number, times int) []Number {
+
+	// fmt.Println("ordered 0 times:")
+	// fmt.Println(printNums(nums))
+	for i := 0; i < times; i++ {
+		indexToMove := 0
+		for nMoved := 0; nMoved < len(nums); nMoved++ {
+			for i := 0; i < len(nums); i++ {
+				if nums[i].index == indexToMove {
+					nums = moveNumber(nums, i, nums[i].value)
+					// fmt.Println(printNums(nums))
+					indexToMove++
+					break
+				}
+			}
+		}
+		// fmt.Println("ordered", i+1, "times:")
+		// fmt.Println(printNums(nums))
+	}
+
+	return nums
+}
+
+func moveNumber(nums []Number, index, value int) []Number {
+	if value == 0 {
+		return nums
+	} else if value > 0 {
+		return doForwardSwaps(nums, index, value%(len(nums)-1))
+	} else {
+		return doBackwardSwaps(nums, index, value%(len(nums)-1))
+	}
+}
+
+func doForwardSwaps(nums []Number, index, times int) []Number {
+	swap := reflect.Swapper(nums)
+	for i := 0; i < times; i++ {
+		swap((index+i)%len(nums), (index+i+1)%len(nums))
+	}
+	return nums
+}
+
+func doBackwardSwaps(nums []Number, index, times int) []Number {
+	swap := reflect.Swapper(nums)
+	for i := 0; i < -times; i++ {
+		swap(((index-i)%len(nums)+len(nums))%len(nums), ((index-i-1)%len(nums)+len(nums))%len(nums))
+	}
+	return nums
+}
+
+func printNums(nums []Number) (res string) {
+	for _, n := range nums {
+		res = fmt.Sprint(res, n.value, " ")
+	}
+	return res
+}
+
+func get10002000300(nums []Number) (int, int, int) {
+	var index int
+	for i, n := range nums {
+		if n.value == 0 {
+			index = i
+		}
+	}
+	return (index + 1000) % len(nums), (index + 2000) % len(nums), (index + 3000) % len(nums)
+}
