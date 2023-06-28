8961cb667cd9470ede86050714c7a452686d5101
Luca Favini <luca.favini.02@gmail.com>
2023-06-28 22:04:40 +0200

    2021-day12: `part2` working

--- 2021/day12/day12.js
+++ 2021/day12/day12.js
@@ -1,32 +1,64 @@
+// https://adventofcode.com/2021/day/12
+// https://github.com/Favo02/advent-of-code
+
 const fs = require("fs")
 const path = require("path")
 
 function parseInput(filepath) {
   return fs
-    .readFileSync(path.join(__dirname, filepath), { encoding: "utf-8"})
+    .readFileSync(path.join(__dirname, filepath), { encoding: "utf-8" })
     .split("\n")
     .slice(0, -1)
 }
 
 function buildGraph(lines) {
   const graph = new Map()
-  lines.forEach(l => {
+  lines.forEach((l) => {
     const tokens = l.split("-")
     const [from, to] = tokens
 
-    graph.set(from, [...graph.get(from) ?? [], to])
-    graph.set(to, [...graph.get(to) ?? [], from])
+    graph.set(from, [...(graph.get(from) ?? []), to])
+    graph.set(to, [...(graph.get(to) ?? []), from])
   })
   return graph
 }
 
-const tree = new Map()
+// true => valid for part2, false => not valid for part2
+function checkPart2Valid(arr) {
+  var dupFound
+
+  for (let i = 0; i < arr.length; i++) {
+    const e = arr[i]
+    
+    if (e === e.toLowerCase()) {
+      const occ = arr.filter(el => el === e).length
+      if (occ >= 2) {
+        if (dupFound && dupFound !== e) {
+          // console.log("fail for", e)
+          return false
+        } else {
+          dupFound = e
+        }
+      }
+    }
+  }
+  return true
+}
+
+// const tree = new Map()
 var count = 0
 
 // build tree of possible paths, number of leaves are number of possible paths to reach end
 function buildTree(graph, cur) {
+  // console.log(cur.toString())
   if (cur.includes("end")) {
-    count++
+    if (checkPart2Valid(cur)) {
+      // console.log("accepted", cur.toString())
+      count++
+    } else {
+      // console.log("rejected", cur.toString())
+    }
+    // tree.delete(cur.toString())
     return
   }
 
@@ -34,19 +66,21 @@ function buildTree(graph, cur) {
 
   const childs = []
   // for each adjacent add a path
-  graph.get(curV).forEach(adj => {
-    if (!(adj === adj.toLowerCase() && cur.includes(adj))) {
-      childs.push([...cur, adj])
+  graph.get(curV).forEach((adj) => {
+    if (adj !== "start") {
+      if (!(adj === adj.toLowerCase() && (cur.filter((e) => e === adj).length >= 2))) {
+        childs.push([...cur, adj])
+      }
     }
   })
-  tree.set(cur.toString(), childs)
-  
-  childs.forEach(next => {
+  // tree.set(cur.toString(), childs)
+
+  childs.forEach((next) => {
     buildTree(graph, next)
   })
 }
 
-const lines = parseInput("input.txt")
+const lines = parseInput(process.argv[2])
 // console.log(lines)
 
 const graph = buildGraph(lines)
@@ -54,6 +88,6 @@ const graph = buildGraph(lines)
 
 buildTree(graph, ["start"])
 
-console.log(tree)
+// console.log(tree)
 console.log(count)
 
