419f58f7a0f20097c04827fbd47044db77c9c065
Luca Favini <luca.favini.02@gmail.com>
2023-06-30 16:13:06 +0200

    repo: update `javascript` template

--- template.js
+++ template.js
@@ -1,12 +1,13 @@
 const fs = require("fs")
 const path = require("path")
 
+const lines = parseInput(process.argv[2])
+console.log(lines)
+
+// parse input from file given as process arg
 function parseInput(filepath) {
   return fs
     .readFileSync(path.join(__dirname, filepath), { encoding: "utf-8"})
     .split("\n")
     .slice(0, -1)
 }
-
-const lines = parseInput("input.txt")
-console.log(lines)
