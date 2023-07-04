e0ae68b39ca34c24f360619b651ad324610ede77
Luca Favini <luca.favini.02@gmail.com>
2023-07-04 21:17:08 +0200

    2021-day17: refactor, optimization

--- 2021/day17/day17.js
+++ 2021/day17/day17.js
@@ -2,39 +2,9 @@ const fs = require("fs")
 const path = require("path")
 
 const target = parseInput(process.argv[2])
-// console.log(target)
-
-// const { res, maxY } = canFall(0, 0, 6, 9, target)
-
-// if (res) {
-//   console.log("maxY:", maxY)
-// }
-
-const {my, cont} = tryAll(target)
-console.log(my, cont)
-
-const debug = debugParseInput("out corretto")
-// console.log(debug)
-
-for (let i = 0; i < debug.length; i++) {
-  const { res } = canFall(0, 0, debug[i].x, debug[i].y, target)
-  if (!res) {
-    console.log(debug[i].x, debug[i].y, res)
-  }
-}
-
-console.log(canFall(0,0,7,-1,target))
-
-function debugParseInput(filepath) {
-  return fs
-    .readFileSync(path.join(__dirname, filepath), { encoding: "utf-8"})
-    .split("\n")
-    .slice(0, -1)
-    .map(l => {
-      const [x,y] = l.split(",")
-      return {x: parseInt(x), y: parseInt(y)}
-    })
-}
+const { my, cont } = findAll(target)
+console.log("max y:", my)
+console.log("cont:", cont)
 
 // parse input from file given as process arg
 function parseInput(filepath) {
@@ -44,19 +14,19 @@ function parseInput(filepath) {
     .split("target area:")[1]
     .split(", ")
 
-  const [ xs, xe ] = inp[0].substring(3).split("..")
-  const [ ys, ye ] = inp[1].substring(2).split("..")
+  const [ x1, x2 ] = inp[0].substring(3).split("..")
+  const [ y1, y2 ] = inp[1].substring(2).split("..")
 
   return {
-    xs: parseInt(xs), xe: parseInt(xe),
-    ys: parseInt(ys), ye: parseInt(ye)
+    x1: parseInt(x1), x2: parseInt(x2),
+    y1: parseInt(y1), y2: parseInt(y2)
   }
 }
 
 // return true if point (x,y) is inside target
 function isInsideTarget(target, x, y) {
-  if (x >= target.xs && x <= target.xe) {
-    if (y >= target.ys && y <= target.ye) {
+  if (x >= target.x1 && x <= target.x2) {
+    if (y >= target.y1 && y <= target.y2) {
       return true
     }
   }
@@ -78,14 +48,16 @@ function newPosition(x, y, xvel, yvel) {
 }
 
 // returns true if the point cannot reach the target
-function isOver(target, x, y) {
-  return y < (target.ye-1)
+function isOver(target, x, y, xvel, yvel) {
+  if (y < (target.y2-1)) return true // y already under targer
+  if (xvel <= 0 && x < target.x1) return true // x before target and x velocity not climbing up
+  return false
 }
 
 // returns true if the starting velocities make the point fall into the target
 function canFall(gx, gy, gxvel, gyvel, target) {
   let maxY = 0
-  while(true) {
+  while(!isOver(target, gx, gy, gxvel, gyvel)) {
     const { x, y, xvel, yvel } = newPosition(gx, gy, gxvel, gyvel)
     gx = x
     gy = y
@@ -95,18 +67,17 @@ function canFall(gx, gy, gxvel, gyvel, target) {
     const fall = isInsideTarget(target, gx, gy)
     // console.log(gx, gy, fall, xvel, yvel)
     if (fall) return { res: true, maxY }
-
-    if (isOver(target, gx, gy)) break
   }
   return { res: false }
 }
 
-function tryAll(target) {
+// finds all initial velocities that fall into target. returns number of velocities (cont) and max y reached (my)
+function findAll(target) {
   let my = 0
   let cont = 0
 
-  for (let y = -1000; y < 1000; y++) {
-    for (let x = -1000; x < 1000; x++) {     
+  for (let y = (target.y1)-1; y < 1000; y++) {
+    for (let x = 1; x < 1000; x++) {     
       const { res, maxY } = canFall(0, 0, x, y, target)
       if (res) {
         cont++
