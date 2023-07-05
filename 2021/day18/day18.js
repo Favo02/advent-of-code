f06de20f20d808116dcb6e5c40d47cb1fa925812
Luca Favini <luca.favini.02@gmail.com>
2023-07-05 18:55:33 +0200

    2021-day18: refactor

--- 2021/day18/day18.js
+++ 2021/day18/day18.js
@@ -1,13 +1,13 @@
 const fs = require("fs")
 const path = require("path")
 
-const lines = parseInput(process.argv[2]).map(l => parseToArray(l))
+const lines = parseInput(process.argv[2])
 
-const res = sumAndReduce(lines)
+const res = sumAllAndReduce(lines)
+const mag = magnitude(res)
 
 console.log(res.join(""))
-
-console.log(magnitude(res))
+console.log("magnitude (part1):", mag)
 
 // parse input from file given as process arg
 function parseInput(filepath) {
@@ -15,8 +15,10 @@ function parseInput(filepath) {
     .readFileSync(path.join(__dirname, filepath), { encoding: "utf-8"})
     .split("\n")
     .slice(0, -1)
+    .map(l => parseToArray(l))
 }
 
+// parse string to array (parse numbers to int)
 function parseToArray(expr) {
   let numBuffer = ""
   let arr = []
@@ -38,19 +40,27 @@ function parseToArray(expr) {
     }
   }
 
-  // console.log(arr)
   return arr
 }
 
-function sum(arr1, arr2) {
-  return [ '[', ...arr1, ',', ...arr2, ']' ]
+// sum two expressions and return new expression
+function sum(expr1, expr2) {
+  return [ '[', ...expr1, ',', ...expr2, ']' ]
 }
 
+// reduce to normal form an expression
 function reduce(expr) {
   let cont = 0
 
   loopExplode:
   for (let i = 0; i < expr.length; i++) {
+
+    // reset this loop
+    function reset() {
+      cont = 0
+      i = -1
+    }
+
     const element = expr[i]
     
     if (element == '[') cont++
@@ -61,11 +71,11 @@ function reduce(expr) {
       // console.log("bef explode: \n", expr.join(""))
       expr = explode(expr, i)
       // console.log("aft explode: \n", expr.join(""), "\n")
-      i = -1
-      cont = 0
+      reset()
       continue
     }
 
+    // check for possible splits only if no explode left
     if (i == expr.length -1) {
 
       for (let j = 0; j < expr.length; j++) {
@@ -74,9 +84,9 @@ function reduce(expr) {
           // console.log("bef split: \n", expr.join(""))
           expr = split(expr, j)
           // console.log("aft split: \n", expr.join(""), "\n")
-          i = -1
-          j = -1
-          cont = 0
+
+          // reset loop and start checking for new explode or slipts
+          reset()
           continue loopExplode
         }
       }
@@ -86,11 +96,11 @@ function reduce(expr) {
   return expr
 }
 
+// explode the couple starting at start index
 function explode(expr, start) { 
   const end = start+5
   
   const elem = expr.slice(start, end)
-  // console.log(elem)
 
   // find number before
   let befInd = -1
@@ -112,52 +122,43 @@ function explode(expr, start) {
   }
   // console.log("aftind", aftInd, expr[aftInd])
 
-  if (befInd != -1) {
-    // console.log("be", expr)
-    expr[befInd] += elem[1]
-    // console.log("ae", expr)
-  }
-  if (aftInd != -1) {
-    expr[aftInd] += elem[3]
-  }
+  if (befInd != -1) expr[befInd] += elem[1]
+  if (aftInd != -1) expr[aftInd] += elem[3]
 
   return [...expr.slice(0, start), 0, ...expr.slice(end)]
 }
 
+// split the couple starting at start index
 function split(expr, start) {
-  // console.log(expr[start])
-
   const a = Math.floor(expr[start]/2)
   const b = Math.ceil(expr[start]/2)
-
-  // console.log("-----", expr[start], a, b)
-  // console.log(a,b)
 
   return [ ...expr.slice(0, start), '[', a, ',', b, ']', ...expr.slice(start+1) ]
 }
 
-function sumAndReduce(lines) {
-  let expr = reduce(lines[0])
+// sums all expressions and return reduced final expression
+function sumAllAndReduce(exprs) {
+  let expr = reduce(exprs[0])
   
-  for (let i = 1; i < lines.length; i++) {
-    // expr = reduce(expr)
-    expr = sum(expr, lines[i])
+  for (let i = 1; i < exprs.length; i++) {
+    expr = sum(expr, exprs[i])
     expr = reduce(expr)
   }
 
   return expr
 }
 
-function magnitude(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i]
+// return magnitude of expression
+function magnitude(expr) {
+  for (let i = 0; i < expr.length; i++) {
+    const element = expr[i]
 
     if (element == ']') {
-      const magnitude = arr[i-1]*2 + arr[i-3]*3
-      arr = [...arr.slice(0, i-4), magnitude, ...arr.slice(i+1)]
+      const magnitude = expr[i-1]*2 + expr[i-3]*3
+      expr = [...expr.slice(0, i-4), magnitude, ...expr.slice(i+1)]
       i = -1
     }
   }
 
-  return arr[0]
+  return expr[0]
 }
