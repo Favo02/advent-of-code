c4b4cde8c363450c6907301097bef137c65c8d52
Luca Favini <luca.favini.02@gmail.com>
2023-12-01 16:48:44 +0100

    add `python` `utils`

---
+++ utils/fast.py
@@ -0,0 +1,17 @@
+import advent
+
+advent.setup(202, 0)
+fin = advent.get_input()
+lines = advent.get_lines(fin)
+
+part1 = 0
+part2 = 0
+
+for line in lines:
+  print(line)
+
+advent.print_answer(1, part1)
+# advent.submit_answer(1, part1)
+
+advent.print_answer(2, part2)
+# advent.submit_answer(2, part2)
