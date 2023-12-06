722cee6181baee1255e33dbfc9b364042803d962
Luca Favini <luca.favini.02@gmail.com>
2023-12-06 13:32:07 +0100

    refactor `2023/day06` `python` solution

--- 2023/day06/day06.py
+++ 2023/day06/day06.py
@@ -2,6 +2,7 @@
 # https://github.com/Favo02/advent-of-code
 
 import sys
+from math import sqrt, floor
 fin = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 
 part1 = 1
@@ -13,17 +14,24 @@ p1records = [int(n) for n in input().split()[1:]]
 p2time = int("".join(map(str, p1times)))
 p2record = int("".join(map(str, p1records)))
 
-def bruteforce(time, rec):
-  count = 0
-  for start in range(time):
-    if start*(time-start) > rec:
-      count += 1
-  return count
+def equation(time, rec):
+  # (time +- (time^2 - 4*rec)) / 2
+  delta = sqrt(time**2 - 4*rec)
+  x1 = (time + delta) / 2
+  x2 = (time - delta) / 2
+
+  # rounding to int
+  x1 = floor(x1)-1 if x1 == floor(x1) else floor(x1)
+  x2 = max(0, floor(x2))
+
+  return (x1, x2)
 
 for time, rec in zip(p1times, p1records):
-  part1 *= bruteforce(time, rec)
+  x1, x2 = equation(time, rec)
+  part1 *= (x1-x2)
 
-part2 = bruteforce(p2time, p2record)
+x1, x2 = equation(p2time, p2record)
+part2 = x1-x2
 
 print("Part 1:", part1)
 print("Part 2:", part2)
