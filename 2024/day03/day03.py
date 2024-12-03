fe8eaf89f89c5a2b7b74d26ba3793f77ddc05efa
Luca Favini <luca.favini.02@gmail.com>
2024-12-04 00:09:56 +0100

    update 2024/day03 `python` solution

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -5,45 +5,22 @@ import re
 import sys
 fin = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 
+part1 = part2 = 0
+enabled = True
+regex = re.compile(r'(mul|do(n\'t)?)\(((\d{1,3}),(\d{1,3}))?\)')
+
 lines = []
 for line in sys.stdin:
-  lines.append(line.strip())
+  for matchh in regex.finditer(line.strip()):
+    match matchh.group(1):
 
-matches = []
-MUL = re.compile(r'mul\((\d+),(\d+)\)')
-DO = re.compile(r'do\(\)')
-DONT = re.compile(r'don\'t\(\)')
+      case "mul":
+        val = int(matchh.group(4)) * int(matchh.group(5))
+        part1 += val
+        if enabled: part2 += val
 
-for y, line in enumerate(lines):
-  for match in MUL.finditer(line):
-    i = match.start()
-    a, b = map(int, (match.group(1), match.group(2)))
-    if not (1 <= a <= 999): continue
-    if not (1 <= b <= 999): continue
-    matches.append(((y, i), "MUL", (a, b)))
-
-for y, line in enumerate(lines):
-  for match in DO.finditer(line):
-    i = match.start()
-    matches.append(((y, i), "DO", (0, 0)))
-
-for y, line in enumerate(lines):
-  for match in DONT.finditer(line):
-    i = match.start()
-    matches.append(((y, i), "DONT", (0, 0)))
-
-matches.sort()
-
-part1 = part2 = 0
-valid = True
-
-for (_, what, (a, b)) in matches:
-  if what == "DO": valid = True
-  elif what == "DONT": valid = False
-
-  if what == "MUL":
-    part1 += a * b
-    if valid: part2 += a * b
+      case "do": enabled = True
+      case "don't": enabled = False
 
 print("Part 1:", part1)
 print("Part 2:", part2)
